094c39e59fc1c834345d5f1dbd928d051b987f75
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-16 12:56:58 +0200

    Refactor email.js to use email template and updated mailtrap configuration

---
+++ backend/mailtrap/email.js
@@ -0,0 +1,18 @@
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTamplate"
+import { MailTrapClient, sender } from "./mailtrap.config.js"
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+    const recipient = [{email}]
+    try {
+        const response = await MailTrapClient.send({
+            from: sender,
+            to: recipient,
+            subject : "Verify your email",
+            html : VERIFICATION_EMAIL_TEMPLATE.replace("{ }", verificationToken),
+            category: "Verification Email",
+        })
+        console.log("Email sent successfully", response);
+    } catch (error) {
+        console.error("Error sending email", error);
+    }
+} 
