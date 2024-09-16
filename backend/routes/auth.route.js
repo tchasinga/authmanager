838b7d2e610952afe443387353919697610e985b
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-16 09:28:04 +0200

    Refactor auth.route.js to add signup, signin, and signout routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express";
+import { singout, singup, sinigin } from "../controller/auth.controller.js";
 
 
 // initialize express 
 const router = express.Router()
 
-
+router.post('singup', singup)
+router.post('login', sinigin)
+router.get('logout', singout)
 
 // exporting side of router
 export default router
