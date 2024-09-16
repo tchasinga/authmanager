ad87c6c573025308d57943460d00c3569fa33647
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-16 09:31:59 +0200

    Add user model with email and password fields

---
+++ backend/models/user.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type:String, 
+        require: true,
+    }
+},{timestamps: true})
