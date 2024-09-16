e3485a53d339208a3b9edd5c7674eafad1abb55d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-16 09:25:48 +0200

    Refactor auth.controller.js to add signup, signin, and signout functionality

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcryptjs";
+import crypto from 'crypto'
+
+export const singup = async(req, res) =>{
+} 
+
+
+export const sinigin = async(req, res) =>{
+} 
+
+
+export const singout = async(req, res) =>{
+} 
