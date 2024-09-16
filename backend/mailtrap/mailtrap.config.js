3d27de90c9e6fddfd9bbb019a1ac3a7cb392fe34
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-16 12:36:54 +0200

    Refactor mailtrap.config.js to comment out recipients array

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -2,26 +2,26 @@ import { MailtrapClient } from "mailtrap";
 
 const TOKEN = "1948b13dc28d3ffcded06f7fec4506d3";
 
-const client = new MailtrapClient({
+export const client = new MailtrapClient({
   token: TOKEN,
 });
 
-const sender = {
+ export const sender = {
   email: "mailtrap@demomailtrap.com",
   name: "Mailtrap Test",
 };
-const recipients = [
-  {
-    email: "tchasingajacques@gmail.com",
-  }
-];
 
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    text: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
+// const recipients = [
+//   {
+//     email: "tchasingajacques@gmail.com",
+//   }
+// ];
+// client
+//   .send({
+//     from: sender,
+//     to: recipients,
+//     subject: "You are awesome!",
+//     text: "Congrats for sending test email with Mailtrap!",
+//     category: "Integration Test",
+//   })
+//   .then(console.log, console.error);
