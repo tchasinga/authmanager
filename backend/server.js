cb0d75bcd76d5aa40fe17a3d6f63d73118256974
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-16 09:22:47 +0200

    Refactor server.js to add API authentication route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoconnection from "./db/mongoconnection.js";
+import authRoutes from './routes/auth.route.js'
 
 
 // initate express
