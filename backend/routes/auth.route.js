c6c36da22819d014b476866926cf2e4333fedb10
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-16 09:44:55 +0200

    Refactor route paths in auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,9 +5,9 @@ import { singout, singup, sinigin } from "../controller/auth.controller.js";
 // initialize express 
 const router = express.Router()
 
-router.post('singup', singup)
-router.post('login', sinigin)
-router.get('logout', singout)
+router.post('/singup', singup)
+router.post('/login', sinigin)
+router.get('/logout', singout)
 
 // exporting side of router
 export default router
