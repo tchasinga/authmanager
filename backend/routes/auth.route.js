0de82764365312f8086253c426a924c4ab5b1f51
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-17 08:37:58 +0200

    Add email verification route to authentication API

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,9 +5,11 @@ import { singout, signup, sinigin } from "../controller/auth.controller.js";
 // initialize express 
 const router = express.Router()
 
-router.post('/singup', signup)
+router.post('/singup', signup) 
 router.post('/login', sinigin)
 router.post('/logout', singout)
+
+router.post('/verify-email', verifyEmail)
 
 // exporting side of router
 export default router
