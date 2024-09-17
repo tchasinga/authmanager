e9c8391dfea9bbe3dfad96801a5631b56023c66f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-17 08:08:10 +0200

    Refactor sendVerificationEmail function for improved readability and formatting

--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -1,18 +1,21 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTamplate"
-import { MailTrapClient, sender } from "./mailtrap.config.js"
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTamplate";
+import { MailTrapClient, sender } from "./mailtrap.config.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
-    const recipient = [{email}]
-    try {
-        const response = await MailTrapClient.send({
-            from: sender,
-            to: recipient,
-            subject : "Verify your email",
-            html : VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
-            category: "Verification Email",
-        })
-        console.log("Email sent successfully", response);
-    } catch (error) {
-        console.error("Error sending email", error);
-    }
-} 
+  const recipient = [{ email }];
+  try {
+    const response = await MailTrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Verify your email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationToken
+      ),
+      category: "Verification Email",
+    });
+    console.log("Email sent successfully", response);
+  } catch (error) {
+    console.error("Error sending email", error);
+  }
+};
