5324f68e70af57dc840c95e8879f4eceabb01dc6
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-17 11:01:25 +0200

    Add functionality to send password reset success email

--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -1,5 +1,6 @@
 import {
   PASSWORD_RESET_REQUEST_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
   VERIFICATION_EMAIL_TEMPLATE,
 } from "./emailTamplate.js";
 import { MailTrapClient, sender } from "./mailtrap.config.js";
@@ -64,5 +65,21 @@ export const sendPasswordResetEmail = async (email, resetURL) => {
 };
 
 export const sendResetSuccessEmail = async (email) => {
-    
+    const recipient = [{ email }];
+
+	try {
+		const response = await MailTrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: "Password Reset Successful",
+			html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+			category: "Password Reset",
+		});
+
+		console.log("Password reset email sent successfully", response);
+	} catch (error) {
+		console.error(`Error sending password reset success email`, error);
+
+		throw new Error(`Error sending password reset success email: ${error}`);
+	}
 };
