31e3896077ac39561987e72137977b065564f9ff
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-17 09:38:14 +0200

    Implement login functionality with error handling and user last login update

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -84,13 +84,40 @@ export const verifyEmail = async (req, res) =>{
 }
 
 export const sinigin = async(req, res) =>{
-	res.clearCookie("token");
-	res.status(200).json({ success: true, message: "Logged out successfully" });
+	const { email, password } = req.body;
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+		const isPasswordValid = await bcryptjs.compare(password, user.password);
+		if (!isPasswordValid) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+
+		generateTokenAndSetCookie(res, user._id);
+
+		user.lastLogin = new Date();
+		await user.save();
+
+		res.status(200).json({
+			success: true,
+			message: "Logged in successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		});
+	} catch (error) {
+		console.log("Error in login ", error);
+		res.status(400).json({ success: false, message: error.message });
+	}
 } 
 
 
 export const singout = async(req, res) =>{
-	
+	res.clearCookie("token");
+	res.status(200).json({ success: true, message: "Logged out successfully" });
 } 
 
 
