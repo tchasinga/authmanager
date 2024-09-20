d9ebb902d77d66a27e2da63add912e8ea3397099
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 08:17:22 +0200

    Enhance welcome heading with styling and cursor pointer in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div>
-      <h1 className="text-4xl font-bold">Welcom now </h1>
+      <h1 className="text-4xl font-bold hover:cursor-pointer bg-blue-600 p-4 rounded-xls">Welcom now </h1>
     </div>
   )
 }
