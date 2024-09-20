04a85a3009fae130dcd7462c88049cbbabdc8d75
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 12:42:05 +0200

    Update signup button styles for improved alignment and responsiveness

--- frontend/src/components/Singup.jsx
+++ frontend/src/components/Singup.jsx
@@ -76,7 +76,7 @@ export default function Singup() {
           className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
           font-bold rounded-lg shadow-lg hover:from-green-600
           hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
-           focus:ring-offset-gray-900 transition duration-200'
+           focus:ring-offset-gray-900 transition duration-200 flex justify-center items-center'
           whileHover={{ scale: 1.02 }}
           whileTap={{ scale: 0.98 }}
           type='submit'
