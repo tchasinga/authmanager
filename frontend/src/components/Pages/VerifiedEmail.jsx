f8e16acf3c4cd955017e608d7ef6774bc603b1e5
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 11:40:20 +0200

    Update VerifiedEmail component title for clarity and styling

--- frontend/src/components/Pages/VerifiedEmail.jsx
+++ frontend/src/components/Pages/VerifiedEmail.jsx
@@ -17,7 +17,9 @@ export default function VerifiedEmail() {
 				transition={{ duration: 0.5 }}
 				className='bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-2xl p-8 w-full max-w-md'
 			>
-                <h1>Checking emai</h1>   
+                <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
+					Verify Your Email
+				</h2>  
             </motion.div>
    </div>           
   )
