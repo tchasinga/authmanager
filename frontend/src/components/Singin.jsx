db4496dc30064e61ff7b00e4cb62c591b77e1d93
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 10:38:16 +0200

    Add state management for email and password in Singin component

--- frontend/src/components/Singin.jsx
+++ frontend/src/components/Singin.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 
 export default function Singin() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
         <h1>Singin side</h1>
