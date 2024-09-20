2666f62c54e19873b05c809a7445925e3533b730
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 12:34:37 +0200

    Add CORS middleware to server.js for cross-origin requests

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import mongoconnection from "./db/mongoconnection.js";
 import authRoutes from './routes/auth.route.js'
 
@@ -12,6 +14,18 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "*",
+    ],
+    credentials: true,
+    allowedHeaders: "*", // or list specific headers if needed
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+  })
+);
 
 
 
