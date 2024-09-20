cd19fb33cc72fb2a23cab2cc7cbbd352412e7102
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 09:18:11 +0200

    Update App component to use min-h-screen for better height management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
 
   return (
-    <div className="max-h-screen bg-gradient-to-br from-gray-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
-      <h1 className="text-4xl font-bold text-red-500">Welcom now is good </h1>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+     
     </div>
   )
 }
