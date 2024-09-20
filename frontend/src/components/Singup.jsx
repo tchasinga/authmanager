ab4d6f42364026fa2913a6d6812b63a21a7bff04
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 10:07:04 +0200

    Add submit button with animations to Signup form

--- frontend/src/components/Singup.jsx
+++ frontend/src/components/Singup.jsx
@@ -50,6 +50,19 @@ export default function Singup() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+
+          {/* Password regex */}
+          <motion.button
+          className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+          font-bold rounded-lg shadow-lg hover:from-green-600
+          hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+           focus:ring-offset-gray-900 transition duration-200'
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.98 }}
+          type='submit'
+          >
+            Submit
+          </motion.button>
         </form>
       </div>
     </motion.div>
