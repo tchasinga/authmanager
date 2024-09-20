b03844d36b359558ef5e2b5ed066f03b7a626e85
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 11:17:44 +0200

    Add loading state to Singin component button

--- frontend/src/components/Singin.jsx
+++ frontend/src/components/Singin.jsx
@@ -9,6 +9,7 @@ export default function Singin() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const isLoading = true
 
   const handlerSubmitHereLogin = (e) => {
     e.preventDefault();
@@ -52,12 +53,14 @@ export default function Singin() {
           className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
           font-bold rounded-lg shadow-lg hover:from-green-600
           hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
-           focus:ring-offset-gray-900 transition duration-200'
+           focus:ring-offset-gray-900 transition duration-200 flex justify-center items-center'
           whileHover={{ scale: 1.02 }}
           whileTap={{ scale: 0.98 }}
           type='submit'
           >
-            Sing In
+            {
+              isLoading ? <Loader/> : "Sing In"
+            }
           </motion.button>
         </form>
 
