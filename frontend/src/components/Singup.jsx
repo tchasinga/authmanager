1e7b411f910adc10dd63a407ba14111ca2c96959
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 09:56:28 +0200

    Add email input field and state management to Signup form

--- frontend/src/components/Singup.jsx
+++ frontend/src/components/Singup.jsx
@@ -1,13 +1,15 @@
 import { motion } from "framer-motion";
 import Inputs from "./Pages/Inputs";
-import {User} from 'lucide-react'
+import { User, Mail} from "lucide-react";
+import { useState } from "react";
 
 export default function Singup() {
-
   const handlerSubmitHere = (e) => {
     e.preventDefault();
     console.log("Form submitted");
-  }
+  };
+
+  const [name, setName] = useState("");
 
   return (
     <motion.div
@@ -16,21 +18,30 @@ export default function Singup() {
       transition={{ duration: 0.5 }}
       className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl shadow-lg rounded-2xl p-6 overflow-hidden"
     >
-     <div className="p-8">
-        <h1 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">Create an account</h1>
+      <div className="p-8">
+        <h1 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          Create an account
+        </h1>
 
         {/* Adding a form here */}
         <form onSubmit={handlerSubmitHere}>
-          
           <Inputs
-             icon={User} 
-             type="text"
-              placeholder="Full name"
+            icon={User}
+            type="text"
+            placeholder="Full name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
 
-          
+          <Inputs
+            icon={Mail}
+            type="email"
+            placeholder="Enter your email"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </form>
-     </div>
+      </div>
     </motion.div>
   );
 }
