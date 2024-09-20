2872f5562546d8aa5f9dea9e7595a5d8a4f3d663
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 12:45:04 +0200

    Set axios to include credentials by default for authentication requests

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 import axios from "axios";
 
+axios.defaults.withCredentials= true;
+
 export const useAuthStore = create((set) => ({
     user: null,
 	isAuthenticated: false,
