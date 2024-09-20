64e4642a600e008741302011d2b76eac052adaf6
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 09:44:23 +0200

    Add form submission handler and update Signup component layout

--- frontend/src/components/Singup.jsx
+++ frontend/src/components/Singup.jsx
@@ -1,6 +1,12 @@
 import { motion } from "framer-motion";
 
 export default function Singup() {
+
+  const handlerSubmitHere = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  }
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
@@ -8,7 +14,12 @@ export default function Singup() {
       transition={{ duration: 0.5 }}
       className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl shadow-lg rounded-2xl p-6 overflow-hidden"
     >
-      <h1>Singup side</h1>
+     <div className="p-8">
+        <h1 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">Create an account</h1>
+
+        {/* Adding a form here */}
+        <form onSubmit={handlerSubmitHere}></form>
+     </div>
     </motion.div>
   );
 }
