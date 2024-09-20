ffe1622e4add756311e65a552a04ce97a34ffd49
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 09:32:15 +0200

    Add framer-motion for animations and implement routing with Home, Signup, and Signin components

---
+++ frontend/src/components/Singin.jsx
@@ -0,0 +1,8 @@
+
+export default function Singin() {
+  return (
+    <div>
+        <h1>Singin side</h1>
+    </div>
+  )
+}
---
+++ frontend/src/components/Singup.jsx
@@ -0,0 +1,8 @@
+
+export default function Singup() {
+  return (
+    <div>
+        <h1>Singup side</h1>
+    </div>
+  )
+}
