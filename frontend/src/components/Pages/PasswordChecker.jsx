3795719bfc819d6aff4284575e4e7eaee6ca6716
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 10:26:09 +0200

    Remove unused import of CheckCheck from PasswordChecker component

--- frontend/src/components/Pages/PasswordChecker.jsx
+++ frontend/src/components/Pages/PasswordChecker.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-
-import { CheckCheck } from "lucide-react";
 import { Check, X } from "lucide-react";
 
 
