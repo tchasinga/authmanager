d333a2569b8c50bd8eb0c98789ebcaa129b039e4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-20 11:39:41 +0200

    Enhance VerifiedEmail component with input handling and animation

--- frontend/src/components/Pages/VerifiedEmail.jsx
+++ frontend/src/components/Pages/VerifiedEmail.jsx
@@ -1,8 +1,24 @@
+import { useRef, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import {motion} from 'framer-motion'
 
 export default function VerifiedEmail() {
+    
+    const [code, setCode] = useState(["","","","","",""])
+    const inputsRef = useRef([])
+    const navigate = useNavigate()
+
+    
   return (
-    <div>
-        <h1>Checking emails</h1>      
-    </div>
+    <div className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'>
+			<motion.div
+				initial={{ opacity: 0, y: -50 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5 }}
+				className='bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-2xl p-8 w-full max-w-md'
+			>
+                <h1>Checking emai</h1>   
+            </motion.div>
+   </div>           
   )
 }
