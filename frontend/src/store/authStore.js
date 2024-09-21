84d29a13519a9b5201ca3bc1bff4ddc85e4b7866
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-21 12:56:52 +0200

    Refactor signup function in authStore for improved formatting and consistency

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,32 +1,23 @@
 import { create } from "zustand";
 import axios from "axios";
 
-export const useAuthStore = create((set) => ({
-  user: null,
-  isAuthenticated: false,
-  error: null,
-  isLoading: false,
-  isCheckingAuth: true,
-  message: null,
 
-  signup: async (email, password, name) => {
-    set({ isLoading: true, error: null });
-    try {
-      const response = await axios.post(
-        `http://localhost:5000/api/auth/singup`,
-        { email, password, name }
-      );
-      set({
-        user: response.data.user,
-        isAuthenticated: true,
-        isLoading: false,
-      });
-    } catch (error) {
-      set({
-        error: error.response.data.message || "Error signing up",
-        isLoading: false,
-      });
-      throw error;
-    }
-  },
+export const useAuthStore = create((set) => ({
+    user: null,
+	isAuthenticated: false,
+	error: null,
+	isLoading: false,
+	isCheckingAuth: true,
+	message: null,
+
+    signup: async (email, password, name) => {
+		set({ isLoading: true, error: null });
+		try {
+			const response = await axios.post(`http://localhost:5000/api/auth/singup`, { email, password, name });
+			set({ user: response.data.user, isAuthenticated: true, isLoading: false });
+		} catch (error) {
+			set({ error: error.response.data.message || "Error signing up", isLoading: false });
+			throw error;
+		}
+	},
 }));
