b623f662913973071dc4d1a66f0bb61ec54cb3a9
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-23 10:58:54 +0200

    Refactor App component to remove unused variables and enhance Home component with user profile and logout functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,7 +39,7 @@ const ProtectedRoute = ({ children }) => {
 };
 
 function App() {
-  const {isCheckingAuth, isAuthenticated, checkAuth, user} = useAuthStore();
+  const {isCheckingAuth, checkAuth} = useAuthStore();
 
   useEffect(() => {
     checkAuth();
@@ -47,8 +47,6 @@ function App() {
 
   if (isCheckingAuth) return <LoadingSpinner />;
 
-  console.log("data is there", isAuthenticated);
-  console.log("user is added", user)
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
