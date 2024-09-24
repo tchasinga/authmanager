9a62c6d4f2d461607c71d9a5cc17c4471c57188d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-24 09:07:37 +0200

    Add Resetpassword component and route for password reset functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import VerifiedEmail from './components/Pages/VerifiedEmail.jsx'
 import { useAuthStore } from './store/authStore.js';
 import { useEffect } from 'react';
 import LoadingSpinner from './components/Pages/LoadingSpinner.jsx';
+import Resetpassword from './components/Resetpassword.jsx';
 
 const RedirectAuthenticatedUser = ({ children }) => {
 	const { isAuthenticated, user } = useAuthStore();
@@ -76,6 +77,7 @@ function App() {
             </RedirectAuthenticatedUser>
           } />
           <Route path='/verify-email' element={<VerifiedEmail />}/>
+          <Route path='/reset-password' element={<Resetpassword />}/>
         </Routes>
     </div>
   )
---
+++ frontend/src/components/Resetpassword.jsx
@@ -0,0 +1,8 @@
+
+export default function Resetpassword() {
+  return (
+    <div>
+      <h1 className="">this is reset password page</h1>
+    </div>
+  )
+}
